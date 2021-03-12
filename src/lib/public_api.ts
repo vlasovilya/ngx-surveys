1dcb410fb019a64794d3b077e69fb9f4aec1db7c
Ilya Vlasov <i.vlasov@me.com>
2021-03-12 15:50:38 +0300

    Angular 11 and Ivy support

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,2 +1,5 @@
 export * from './ngx-survey/ngx-survey.component';
 export * from './ngx-survey/ngx-survey.module';
+export * from './ngx-survey/form-builder/form-builder.component';
+export * from './ngx-survey/form-item/form-item.component';
+export * from './ngx-survey/form-item/form-item';
