636376d89f7a09ae3e00ca18f18833ce2e3351f0
Ilya Vlasov <i.vlasov@me.com>
2022-05-02 14:40:42 +0400

    added service to exports list

--- projects/ngx-surveys/src/public-api.ts
+++ projects/ngx-surveys/src/public-api.ts
@@ -1,3 +1,4 @@
+export * from './lib/ngx-survey.service';
 export * from './lib/ngx-survey.component';
 export * from './lib/ngx-survey.module';
 export * from './lib/form-builder/form-builder.component';
