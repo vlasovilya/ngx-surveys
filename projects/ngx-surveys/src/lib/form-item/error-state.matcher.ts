c26f0a5f8fb37f43b4d89c152cdf45a515a0c41e
Ilya Vlasov <i.vlasov@me.com>
2024-11-14 16:44:42 +0200

    Angular 14 update

--- projects/ngx-surveys/src/lib/form-item/error-state.matcher.ts
+++ projects/ngx-surveys/src/lib/form-item/error-state.matcher.ts
@@ -1,15 +1,15 @@
-import {FormControl, FormGroupDirective, NgForm} from '@angular/forms';
+import {UntypedFormControl, FormGroupDirective, NgForm} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 
 export class SurveyErrorStateMatcher implements ErrorStateMatcher {
     public item:any;
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(control: UntypedFormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     return !(this.item && !(this.item.errors && this.item.errors.length));
   }
 }
 
 export class ItemOptionStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl, form: FormGroupDirective | NgForm): boolean {
+  isErrorState(control: UntypedFormControl, form: FormGroupDirective | NgForm): boolean {
     //console.log(control);
     return control && !control.value;
   }
